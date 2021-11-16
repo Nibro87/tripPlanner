1c97f8519052851f51f5df89079a456937cfd71a
Nibro87 <71063030+Nibro87@users.noreply.github.com>
2021-11-16 19:39:12 +0100

    update

--- src/main/java/rest/NewsResource.java
+++ src/main/java/rest/NewsResource.java
@@ -6,7 +6,7 @@ import com.google.gson.GsonBuilder;
 import entities.DTO.ArticleDTO;
 import facades.FacadeExample;
 import utils.EMF_Creator;
-import utils.Env;
+import utils.Environment;
 import utils.HttpUtils;
 
 import javax.persistence.EntityManagerFactory;
@@ -14,9 +14,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.util.List;
 
 @Path("news")
 public class NewsResource {
@@ -38,7 +36,7 @@ public class NewsResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Path("headlines")
     public String getNews() throws IOException {
-        String url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" +Env.APIKEY;
+        String url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + Environment.APIKEY;
        String headlines = HttpUtils.fetchData(url);
        ArticleDTO articleDTO = GSON.fromJson(headlines,ArticleDTO.class);
 
--- src/main/java/utils/Env.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class Env {
-
-    public final static String APIKEY = "48ea0038d21a4d90928f535207ea7191";
-
-
-}
