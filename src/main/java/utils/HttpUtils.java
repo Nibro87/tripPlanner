5d395b9d521cfa213cfd701ff0ec837c5a95fccc
Nibro87 <71063030+Nibro87@users.noreply.github.com>
2021-11-16 01:08:56 +0100

    Update HttpUtils.java

--- src/main/java/utils/HttpUtils.java
+++ src/main/java/utils/HttpUtils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -13,17 +14,20 @@ public class HttpUtils {
         URL url = new URL(_url);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
-        //con.setRequestProperty("Accept", "application/json;charset=UTF-8");
-        con.setRequestProperty("Accept", "application/json");
+        con.setRequestProperty("Accept", "application/json;charset=UTF-8");
+        //con.setRequestProperty("Accept", "application/json");
         con.setRequestProperty("User-Agent", "server");
 
-        Scanner scan = new Scanner(con.getInputStream());
+        Scanner scan = new Scanner(con.getInputStream(),"utf-8");
         String jsonStr = null;
         if (scan.hasNext()) {
             jsonStr = scan.nextLine();
         }
         scan.close();
+
+
         return jsonStr;
     }
+
 
 }
