dbafdb87cc624e098a822bcb7cc53e81feb0340b
Nibro87 <71063030+Nibro87@users.noreply.github.com>
2022-01-19 22:03:22 +0100

    added facade test and rest test

--- src/test/java/rest/TripResourceTest.java
+++ src/test/java/rest/TripResourceTest.java
@@ -1,4 +1,97 @@
 package rest;
 
+import entities.Trip;
+import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.util.HttpStatus;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.EMF_Creator;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+
+import static io.restassured.RestAssured.given;
+
 public class TripResourceTest {
+
+    private static final int SERVER_PORT = 7777;
+    private static String SERVER_URL = "http://localhost/api";
+
+    static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
+
+    private static HttpServer httpServer;
+    private static EntityManagerFactory emf;
+
+    static HttpServer startServer() {
+        ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
+        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+        //This method must be called before you request the EntityManagerFactory
+        EMF_Creator.startREST_TestWithDB();
+        emf = EMF_Creator.createEntityManagerFactoryForTest();
+
+        httpServer = startServer();
+        //Setup RestAssured
+        RestAssured.baseURI = SERVER_URL;
+        RestAssured.port = SERVER_PORT;
+        RestAssured.defaultParser = Parser.JSON;
+    }
+
+    @AfterAll
+    public static void closeTestServer() {
+        //System.in.read();
+
+        //Don't forget this, if you called its counterpart in @BeforeAll
+        EMF_Creator.endREST_TestWithDB();
+        httpServer.shutdownNow();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+       Trip trip = new Trip("testname","testdate","testtime","testlocation","testdur","testpack");
+       Trip trip2 = new Trip("name","date","time","location","duration","packing");
+
+          try {
+            em.getTransaction().begin();
+
+            em.persist(trip);
+            em.persist(trip2);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+
+    }
+
+    @Test
+    public void testServerIsUP(){
+
+        given().when().get("/trip").then().statusCode(200);
+
+    }
+
+    @Test
+    public void testAlltrips(){
+        given().contentType("application/json")
+                .get("trip/alltrips")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.OK_200.getStatusCode());
+
+    }
+
+
+
 }
